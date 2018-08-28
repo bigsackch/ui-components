b012d98a1885077704f1fe5a560eef3239333725
Bjørnar Skog <bjornar@eventum.no>
2018-08-28 15:13:55 +0200

    Fix url-loader so it loads images only

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,31 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  entry: {
-    index: path.resolve(__dirname, 'src/index.js'),
-    Grid: path.resolve(__dirname, 'src/Grid.js'),
-    Header: path.resolve(__dirname, 'src/Header.js')
-},
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    filename: '[name].js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
-    library: '',
-    libraryTarget: 'commonjs'
   },
   externals: [nodeExternals()],
   mode: 'development',
   module: {
     rules: [{
       test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
+      exclude: /node_modules/,
       use: {
         loader: 'babel-loader'
       }
     }, {
-      loader: "url-loader",
-      options: {
-        limit: 8192,
-        fallback: "file-loader",
-        publicPath: `./static/images/`,
-        outputPath: `./lib/static/images/`,
-        name: "[name]-[hash].[ext]"
+      test: /\.(jpe?g|png|svg|gif|ico)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+          fallback: "file-loader",
+          publicPath: `./static/images/`,
+          outputPath: `./lib/static/images/`,
+          name: "[name]-[hash].[ext]"
+        }
       }
     }]
   },
