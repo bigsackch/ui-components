30a9cdd588f7eca52168a84abb4b5684546e0d10
Bjørnar Skog <bjornar@eventum.no>
2018-12-04 10:15:55 +0100

    Fix: Add missing export LocaleSelector in index

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export * from './CopyrightFooter';
 export * from './FormComponents';
 export * from './Grid';
 export * from './Header';
+export * from './LocaleSelector';
 export * from './MainContent';
 export * from './Modal';
 export * from './ModalCloseButton';
