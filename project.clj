85908a369ed03c86ffc3ec533abcfb3b828e5c20
Bjørnar Skog <bjornar@eventum.no>
2018-12-10 18:19:06 +0100

    Version 3.12.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eventum-ui-components "3.11.4"
+(defproject eventum-ui-components "3.12.0"
   :source-paths ["src-cljs"]
   :dependencies [[reagent "0.8.1" :scope "provided"]]
   :plugins [[s3-wagon-private "1.3.2"]]
