847474d75d2d56e2f636071fb8b8e198248a93d6
Bjørnar Skog <bjornar@eventum.no>
2019-01-08 13:05:10 +0100

    Version 4.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eventum-ui-components "4.0.0"
+(defproject eventum-ui-components "4.1.0"
   :source-paths ["src-cljs"]
   :dependencies [[reagent "0.8.1" :scope "provided"]]
   :plugins [[s3-wagon-private "1.3.2"]]
