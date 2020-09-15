f4fe8d3189895059f5b728904f11610b78f84f4b
Bjørnar Skog <bjornar.skog@gocrisp.com>
2020-09-15 11:07:38 +0200

    VERSION 4.5.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eventum-ui-components "4.5.7"
+(defproject eventum-ui-components "4.5.8"
   :source-paths ["src-cljs"]
   :dependencies [[reagent "0.8.1" :scope "provided"]]
   :plugins [[s3-wagon-private "1.3.2"]]
