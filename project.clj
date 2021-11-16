b37d634658719e31dc3571108d3d8647b29b6124
Sergey Pariev <spariev@gmail.com>
2021-11-16 10:32:04 +0200

    Version 4.6.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eventum-ui-components "4.6.4"
+(defproject eventum-ui-components "4.6.6"
   :source-paths ["src-cljs"]
   :dependencies [[reagent "0.8.1" :scope "provided"]]
   :plugins [[s3-wagon-private "1.3.2"]]
