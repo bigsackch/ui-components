0b93dc3201da813f274932de6a0228f415231964
Sergey Pariev <spariev@gmail.com>
2022-01-10 14:04:27 +0200

    Add link to venue transfer page

--- src/HeaderOptionsEventum.js
+++ src/HeaderOptionsEventum.js
@@ -19,6 +19,7 @@ const i18n = {
     host: 'Utleier',
     inbox: 'Meldinger',
     performInvoicing: 'Fakturering',
+    adminTransferVenues: 'Overfør lokaler',
     login: 'Logg inn',
     logout: 'Logg ut',
     menu: 'Meny',
@@ -38,6 +39,7 @@ const i18n = {
     host: 'Host',
     inbox: 'Messages',
     performInvoicing: 'Invoicing',
+    adminTransferVenues: 'Transfer venues',
     login: 'Login',
     logout: 'Log out',
     menu: 'Menu',
@@ -52,6 +54,7 @@ const i18n = {
 const URI = {
   accounts: '/hosting/accounts',
   adminPerformInvoicing: '/hosting/accounting/invoicing',
+  adminTransferVenues: '/hosting/venue-transfer',
   adminStats: '/inbox/admin/stats',
   adminUsers: '/inbox/admin/users',
   becomeHost: {
@@ -88,6 +91,7 @@ function AdminMenuLinks({ locale, onClick }: { locale: string, onClick?: () => v
       <ModalMenuLink href={URI.accounts} onClick={onClick}>{text.accounts}</ModalMenuLink>
       <ModalMenuLink href={URI.adminUsers} onClick={onClick}>{text.users}</ModalMenuLink>
       <ModalMenuLink href={URI.adminPerformInvoicing} onClick={onClick}>{text.performInvoicing}</ModalMenuLink>
+      <ModalMenuLink href={URI.adminTransferVenues} onClick={onClick}>{text. adminTransferVenues}</ModalMenuLink>
       <ModalMenuLink href={URI.adminStats} onClick={onClick}>{text.stats}</ModalMenuLink>
     </React.Fragment>
   )
