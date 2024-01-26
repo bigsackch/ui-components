8069df4aa630ec25cbe16e868fa3d8a3712acf01
fmnoise <fmnoise@gmail.com>
2024-01-26 13:53:01 +0100

    reconfigure repo

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject eventum/ui-components "4.6.7"
   :source-paths ["src-cljs"]
   :dependencies [[reagent "0.8.1" :scope "provided"]]
-  :repositories {"github" {:url "https://maven.pkg.github.com/EventumAS/eventum-ui-components"
+  :repositories {"github" {:url "https://maven.pkg.github.com/bigsackch/ui-components"
                            :username "private-token"
                            :password :env/GITHUB_TOKEN
                            :sign-releases false}})
